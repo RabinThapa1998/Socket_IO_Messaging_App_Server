0b29c5c7586c97d8c4de71d5aad0b67e8f833eec
Rabin Thapa <imrabin1998@gmail.com>
2022-06-19 20:43:52 +0545

    feat: add login route

--- index.js
+++ index.js
@@ -4,13 +4,65 @@ const http = require("http");
 const { Server } = require("socket.io");
 const PORT = 8080;
 const cors = require("cors");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("./model/user");
+require("dotenv").config();
+const DB = process.env.DATABASE;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+
+app.use(express.json());
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
+});
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connnected to db");
+    // app.listen(PORT, () => console.log("connected to server at port", PORT));
+  })
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.post("/", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res
+        .status(422)
+        .json({ error: "please enter username and password" });
+    } else {
+      const checkuser = await User.findOne({ username });
+      if (checkuser) {
+        return res
+          .status(422)
+          .json({ error: "username not available change username" });
+      } else {
+        const hashedpassword = await bcrypt.hash(password, 12);
+        await new User({
+          username,
+          password: hashedpassword,
+        }).save();
+        return res.status(200).json({ message: "user created successfully" });
+      }
+    }
+  } catch (err) {
+    console.log("login error__________________________", err);
+  }
 });
 
 io.on("connection", (socket) => {
